c62b4641d472f05fa0cd18a6dc560879c6971e6e
rubenk <ruben.k@captainfresh.in>
2026-07-28 15:32:15 +0530

    feat(domain): add bookSlot to BookingService interface

---
+++ lib/domain/repositories/booking_service.dart
@@ -0,0 +1,14 @@
+import '../../domain/models/schedule_slot_item.dart';
+import '../../domain/models/turf_summary.dart';
+
+abstract interface class BookingService {
+  Future<TurfSummary> getTurf(String turfId);
+  Future<List<ScheduleSlotItem>> getSchedule({
+    required String turfId,
+    required DateTime date,
+  });
+  Future<ScheduleSlotItem> bookSlot({
+    required String turfId,
+    required String slotId,
+  });
+}
